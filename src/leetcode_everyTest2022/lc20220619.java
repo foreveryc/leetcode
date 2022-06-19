65f44c97db502357e4b09c88386c2229e1b5e682
“yaochen” <1209957987@qq.com>
2022-06-19 23:38:47 +0800

    lc20220619

---
+++ src/leetcode_everyTest2022/lc20220619.java
@@ -0,0 +1,14 @@
+package leetcode_everyTest2022;
+
+/**
+ * 给你一个二叉树的根结点root，请返回出现次数最多的子树元素和。如果有多个元素出现的次数相同，
+ * 返回所有出现次数最多的子树元素和（不限顺序）。
+ * 一个结点的「子树元素和」定义为以该结点为根的二叉树上所有结点的元素之和（包括结点本身）。
+ */
+
+
+public class lc20220619 {
+    public int[] findFrequentTreeSum(TreeNode root) {
+
+    }
+}
